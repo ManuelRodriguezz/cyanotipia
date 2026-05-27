c24ad5ce4fb3e0a197fdff40c70c2f3b7a7a166c
Manuel Rodriguez <tu-email@ejemplo.com>
2026-05-26 22:30:56 -0300

    feat: agregando componente real a conjuntos

---
+++ app/conjuntos/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Conjuntos from '../components/Conjuntos';
+
+export default function ConjuntosPage() {
+  return (
+    <main className="min-h-screen pt-24 bg-[#FAF9F6]">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <Conjuntos />
+      </div>
+    </main>
+  );
+}
